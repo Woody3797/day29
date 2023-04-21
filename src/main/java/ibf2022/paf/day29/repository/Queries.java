a3de759ecd1aaaf2c87eb79e2d201f5d860d0881
Woody3797 <woody3797@gmail.com>
2023-04-21 12:38:40 +0800

    day29revision

--- src/main/java/ibf2022/paf/day29/repository/Queries.java
+++ src/main/java/ibf2022/paf/day29/repository/Queries.java
@@ -3,7 +3,6 @@ package ibf2022.paf.day29.repository;
 public class Queries {
     
     public static final String INSERT_RSVP = """
-        insert into rsvp(name,email,phone,confirmation_date,
-        comments,food_type) values (?,?,?,?,?,?)
+        INSERT INTO rsvp (name, email, phone, confirmation_date, comments, food_type) values (?, ?, ?, ?, ?, ?)
     """;
 }
